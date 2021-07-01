835691ac9cf4a4b6abfef0952e63e9029d1f562f
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-01 15:15:47 -0700

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+//1
+var customerName = "bob";
+console.log("test 1: " + customerName)
+
+//2
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+console.log("test 2: " + upperCaseCustomerName())
+
+//3
+var bestCustomer;
+console.log("test 3: " + bestCustomer)
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+console.log("test 3: " + setBestCustomer())
+
+//4
+function overwriteBestCustomer(value = '') {
+    bestCustomer = value
+    return bestCustomer
+}
+console.log("test 4: " + overwriteBestCustomer('maybe bob'))
+
+//5 
+const leastFavoriteCustomer = "definetly bob"
+console.log("test 5: " + leastFavotriteCustomer)
+function changeLeastFavoriteCustomer() {
+    //leastFavotriteCustomer = 'uncertianly bob'
+    leastFavoriteCustomer = 'uncertianly bob'
+}
+console.log("test  5: " + changeLeastFavoriteCustomer())
